d708ad2c420b979292a8e52c7f6a908ad1e11223
Silky Singh <silky1708@gmail.com>
2024-12-12 11:29:02 -0800

    clean up code

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,49 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-// import Editor from '@monaco-editor/react'
-import axios from 'axios'
 import './App.css'
 
 import React from "react";
-// import MonacoEditor from "./MonacoEditor";
-import MinimalCodeEditor from './MinimalCodeEditor'
 import CodeEditorWithSuggestions from './CodeEditorwithSuggestions'
 
 const App = () => {
     return (
         <div>
-            {/* <h1>Python Code Editor with Custom IntelliSense</h1> */}
             <CodeEditorWithSuggestions />
         </div>
     );
 };
 
 export default App;
-
-
-
-
-
-
-
-// function App() {
-//   function handleEditorChange(value, event) {
-//     console.log('here is the current code:', value);
-//   }
-//   return (
-//     <div className='App'>
-//        <Editor 
-//         height="90vh"
-//         width="100%"
-//         theme="vs-dark"
-//         defaultLanguage='python'
-//         onChange={handleEditorChange}
-//         saveViewState={true}
-//        />
-//     </div>
-//   )
-// }
-
-// export default App
